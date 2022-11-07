1ffa2129b6469d42ddc73eabc98a545cf73d4b11
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-07 19:45:03 +0300

    create project with all libraries

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-for="i in aboba">
+    <p>123123</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const aboba = 3;
+</script>
+
+<style scoped lang="scss">
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+
+  &:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  &.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import "@/resetNormalize.css";
+import App from "./App.vue";
+const pinia = createPinia();
+
+createApp(App).use(pinia).mount("#app");
