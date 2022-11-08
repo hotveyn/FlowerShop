cade25603fa43461d7f6b9d1d5f9be0e5b62e198
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-08 20:03:37 +0300

    add empty links in header

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -1,66 +1,70 @@
 <template>
   <header class="header">
-    <RouterLink
-        to="/"
-        class="rt-link">
-      <h1><span class="orange-text">Flower</span> Shop</h1>
-    </RouterLink>
-    <nav>
-      <ul>
-        <RouterLink
-            to="/"
-            :class="{'orange-text':route.name === 'home'}"
-            class="rt-link">
-          <li>Home</li>
-        </RouterLink>
-        <RouterLink
-            to="/"
-            :class="{'orange-text':route.name === 'shop'}"
-            class="rt-link">
-          <li>Shop</li>
-        </RouterLink>
-        <RouterLink
-            to="/"
-            :class="{'orange-text':route.name === 'blog'}"
-            class="rt-link">
-          <li>Blog</li>
-        </RouterLink>
-        <RouterLink
-            to="/"
-            :class="{'orange-text':route.name === 'about'}"
-            class="rt-link">
-          <li>About</li>
-        </RouterLink>
-      </ul>
-    </nav>
-    <div class="reg-cart">
-      <RouterLink to="/">
-        <img src="@/icons/log.svg" alt="log">
+    <div class="header-container">
+      <RouterLink
+          to="/"
+          class="rt-link">
+        <h1><span class="orange-text">Flower</span> Shop</h1>
       </RouterLink>
-      <RouterLink to="/">
-        <img src="@/icons/cart.svg" alt="log">
-      </RouterLink>
+      <nav class="nav-bar">
+        <ul>
+          <RouterLink
+              to="/"
+              :class="{'orange-text':route.name === 'home'}"
+              class="rt-link">
+            <li>Home</li>
+          </RouterLink>
+          <RouterLink
+              to="/"
+              :class="{'orange-text':route.name === 'shop'}"
+              class="rt-link">
+            <li>Shop</li>
+          </RouterLink>
+          <RouterLink
+              to="/"
+              :class="{'orange-text':route.name === 'blog'}"
+              class="rt-link">
+            <li>Blog</li>
+          </RouterLink>
+          <RouterLink
+              to="/"
+              :class="{'orange-text':route.name === 'about'}"
+              class="rt-link">
+            <li>About</li>
+          </RouterLink>
+        </ul>
+      </nav>
+      <div class="reg-cart">
+        <RouterLink to="/">
+          <div class="log-button">
+            <img src="@/icons/log.svg" alt="log">
+          </div>
+        </RouterLink>
+        <RouterLink to="/">
+          <div class="cart-button">
+            <img src="@/icons/cart.svg" alt="log">
+          </div>
+        </RouterLink>
+      </div>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
-import {useRoute} from "vue-router";
+import {useRoute} from 'vue-router';
 
 const route = useRoute();
 </script>
 
-<style lang="scss">
-$myOrange: rgba(255, 143, 82, 1);
-@mixin myFlex($jc:space-between,$ai:center,$fd:row) {
-  display: flex;
-  justify-content: $jc;
-  align-items: $ai;
-  flex-direction: $fd;
-}
+<style scoped lang="scss">
+@import "@/mixins.scss";
 
-.header {
-  @include myFlex($ai:flex-end);
+.header-container {
+
+  @include myFlex($ai: flex-end);
+  max-width: 1200px;
+  width: 100%;
+  margin: 0 auto;
   padding: 30px 0;
   font-family: 'Roboto', sans-serif;
 
@@ -69,28 +73,29 @@ $myOrange: rgba(255, 143, 82, 1);
     color: black;
   }
 
-  .orange-text {
-    color: $myOrange;
-  }
+
 
   h1 {
     font-size: 32px;
     font-weight: 500;
   }
 
-  nav ul {
+  .nav-bar ul{
     @include myFlex();
 
-    .rt-link{
+    .rt-link {
       font-size: 23px;
-    }
-    .rt-link:not(:first-child) {
-      margin-left: 20px;
+      transition: font-weight 0.1s;
+      &:not(:first-child) {
+        margin-left: 20px;
+      }
     }
 
   }
-
-  .reg-cart{
+  .orange-text {
+    color: $myOrange;
+  }
+  .reg-cart {
     @include myFlex();
     width: 70px;
   }
