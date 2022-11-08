ea776f79fd027d25d80f754ab98b6aac4d2780bf
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-09 00:23:38 +0300

    add best sales component

--- src/components/home/BestSales.vue
+++ src/components/home/BestSales.vue
@@ -1,10 +1,25 @@
 <template>
-<div class="best-flowers"/>
+  <div class="best-flowers">
+    <TheFlower
+        v-for="i in flowers"
+        :key="i.id"
+        :flower="i"/>
+  </div>
 </template>
 
 <script setup lang="ts">
+import TheFlower from "@/components/home/TheFlower.vue";
+import {useFlowersStore} from "@/stores/flowers";
 
+const flowersStore = useFlowersStore();
+const flowers = flowersStore.getFlowers();
 </script>
 
 <style scoped lang="scss">
+.best-flowers{
+  gap: 25px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+}
 </style>
