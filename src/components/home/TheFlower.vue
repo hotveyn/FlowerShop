3d2375a1e17049935fce6987f2dcec0a2db97c43
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-09 00:23:29 +0300

    Add flower component

---
+++ src/components/home/TheFlower.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="flower">
+    <img
+        :src="'src/img/' + props.flower.img"
+        :alt="props.flower.name">
+    <p class="flower__name">{{ props.flower.name }}</p>
+    <div class="buy">
+      <p class="buy__price">{{ props.flower.price }}$</p>
+      <button class="buy__add">
+        <img src="@/icons/cart.svg" alt="cart">
+        Add to cart
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {IFlower} from '@/interfaces/IFlower';
+const props = defineProps<{
+  flower: IFlower;
+}>();
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixins.scss";
+
+.flower {
+  background-color: white;
+  padding: 17px;
+  border-radius: 5px;
+  >img{
+    margin-bottom: 9px;
+  }
+  .flower__name{
+    font-size: 26px;
+    margin-bottom: 35px;
+  }
+  .buy{
+    @include myFlex();
+    .buy__price{
+      font-size: 18px;
+      color: rgba(131, 131, 131, 1);
+    }
+    .buy__add{
+      cursor: pointer;
+      border: 0;
+      background-color: white;
+      @include myFlex();
+      color: $myOrange;
+      font-size: 18px;
+      img{
+        height: 20px;
+        margin-right: 5px;
+      }
+    }
+  }
+}
+</style>
--- src/stores/flowers.ts
+++ src/stores/flowers.ts
@@ -1,54 +1,59 @@
-import {defineStore} from 'pinia';
-import {IFlower} from '@/interfaces/IFlower';
+import {defineStore} from "pinia";
+import {IFlower} from "@/interfaces/IFlower";
 
 interface State {
     flowers: IFlower[];
 }
 
-export const useFlowersStore = defineStore('flowers', {
+export const useFlowersStore = defineStore("flowers", {
     state: (): State => ({
         flowers: [
             {
                 id: 1,
-                img: '@/img/periwinkle.png',
-                name: 'Periwinkle',
+                img: "periwinkle.png",
+                name: "Periwinkle",
                 price: 7,
-                description: 'For the Lord takes delight in His people; He crowns the humble and salvation.',
+                description: "For the Lord takes delight in His people; He crowns the humble and salvation.",
                 favorite: false,
                 rate: 0,
                 popular: 3
             },
             {
                 id: 2,
-                img: '@/img/daisy.png',
-                name: 'Daisy',
+                img: "daisy.png",
+                name: "Daisy",
                 price: 5,
-                description: 'For whatever things were written before were written for our learning, that we through the patience and comfort of the Scriptures might have hope.',
+                description: "For whatever things were written before were written for our learning, that we through the patience and comfort of the Scriptures might have hope.",
                 favorite: false,
                 rate: 0,
                 popular: 2
             },
             {
                 id: 3,
-                img: '@/img/sunFlower.png',
-                name: 'Sun flower',
+                img: "sunFlower.png",
+                name: "Sun flower",
                 price: 15,
-                description: 'Even though I walk through the valley of the shadow of death, I fear no evil, for You are with me; Your rod and Your staff, they comfort me.',
+                description: "Even though I walk through the valley of the shadow of death, I fear no evil, for You are with me; Your rod and Your staff, they comfort me.",
                 favorite: false,
                 rate: 0,
                 popular: 1
             },
             {
                 id: 4,
-                img: '@/img/whiteRose.png',
-                name: 'White Rose',
+                img: "whiteRose.png",
+                name: "White Rose",
                 price: 20,
-                description: 'Then shall the righteous shine forth as the sun in the kingdom of the Father.',
+                description: "Then shall the righteous shine forth as the sun in the kingdom of the Father.",
                 favorite: false,
                 rate: 0,
                 popular: 0
             },
         ],
     }),
+    actions:{
+        getFlowers(){
+            return this.flowers;
+        }
+    }
 });
 
