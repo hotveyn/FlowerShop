508240db06867954bd181879632b73b659471e30
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-10 13:09:12 +0300

    Create ShopToolsCheep.vue

---
+++ src/components/shop/ShopToolsCheep.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="shop__cheep shop__button">
+    <img
+        src="@/icons/cheep.png"
+        alt="cheep">
+    Cheep
+  </button>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixins.scss";
+
+.shop__button {
+  padding: 10px 15px;
+  border: 3px solid white;
+  border-radius: 5px;
+  background-color: white;
+  cursor: pointer;
+  @include myFlex();
+  font-size: 19px;
+  transition: border 0.2s;
+  &:hover{
+
+    border: 3px solid $myOrange;
+  }
+  img {
+    margin-right: 5px;
+  }
+}
+</style>
