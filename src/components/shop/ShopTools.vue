0f9d3384233a1d4d3a45a9f5b5cee82fd060b7f1
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-10 13:09:08 +0300

    add ShopTools component

---
+++ src/components/shop/ShopTools.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="shop__tools">
+    <ShopToolsFiltering/>
+    <ShopToolsSearch/>
+    <ShopToolsCheep/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ShopToolsFiltering from "@/components/shop/ShopToolsFiltering.vue";
+import ShopToolsSearch from "@/components/shop/ShopToolsSearch.vue";
+import ShopToolsCheep from  "@/components/shop/ShopToolsCheep.vue";
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixins.scss";
+.shop__tools {
+  @include myFlex();
+  width: 100%;
+  border-radius: 5px;
+  padding: 30px 20px;
+  background-color: white;
+
+}
+</style>
