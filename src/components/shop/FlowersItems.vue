8fc3fa77ed1aef18312b610e74bbc2d62a4b37c7
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-10 13:08:59 +0300

    Add FlowersItems component

---
+++ src/components/shop/FlowersItems.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flowers-items">
+    <TheFlower
+        v-for="i in flowersStore.getFlowers()"
+        :key="i.id"
+        :flower="i"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TheFlower from "@/components/TheFlower.vue";
+import {useFlowersStore} from "@/stores/flowers";
+
+const flowersStore = useFlowersStore();
+</script>
+
+<style scoped lang="scss">
+.flowers-items {
+  margin-top: 40px;
+  gap: 35px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-template-rows: repeat(1, 1fr);
+}
+</style>
