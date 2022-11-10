9057edde40614ef04630aee299aa2f5c71d4a12b
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-10 13:08:32 +0300

    add theShop component

---
+++ src/components/shop/TheShop.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="shop">
+    <ShopTools/>
+    <FlowersItems/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ShopTools from "@/components/shop/ShopTools.vue";
+import FlowersItems from "@/components/shop/FlowersItems.vue";
+</script>
+
+<style scoped lang="scss">
+@import "@/mixins.scss";
+
+.shop {
+  @include myFlex($fd: column);
+  font-family: Roboto, sans-serif;
+
+}
+</style>
