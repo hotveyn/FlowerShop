3c53269973effdff15e4792a2fac7588e00af7d5
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-10 09:46:36 +0300

    add cart store

---
+++ src/stores/cart.ts
@@ -0,0 +1,27 @@
+import {defineStore} from "pinia";
+import {IFlower} from "@/interfaces/IFlower";
+
+interface State {
+    flowersInCart: IFlower[];
+}
+
+
+export const useReviewStore = defineStore("review", {
+    state: (): State => ({
+        flowersInCart: [],
+    }),
+    actions: {
+        getCartLength(): number {
+            return this.flowersInCart.length;
+        },
+        addNewFlowerInCart(newFlower: IFlower): void {
+            this.flowersInCart.push(newFlower);
+        },
+        removeFlowerFromCart(flowerToRemove: IFlower): void {
+            //Fix: make more optimized
+            this.flowersInCart.filter((value) => {
+                return value !== flowerToRemove;
+            });
+        },
+    },
+});
