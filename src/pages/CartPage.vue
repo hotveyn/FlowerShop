642ec8a396288a702aaebd2bc7b62592edbac3c3
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-14 00:58:22 +0300

    Create CartPage.vue

---
+++ src/pages/CartPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="cart-container">
+    <div
+        v-if="cartLen"
+        class="cart-full">
+      <CartFlowers/>
+      <CartBuy/>
+    </div>
+    <div
+        v-else
+        class="cart-empty">
+      <img
+          src="@/assets/img/cartEmpty.png"
+          alt="cart empty">
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useCartStore} from "@/stores/cart";
+import CartFlowers from "@/components/cart/CartFlowers.vue";
+import CartBuy from "@/components/cart/CartBuy.vue";
+import {computed} from "vue";
+
+const cartStore = useCartStore();
+
+const cartLen = computed(() => {
+  return cartStore.getLength;
+});
+
+</script>
+
+<style scoped lang="scss">
+@import "@/mixins.scss";
+
+.cart-container {
+  max-width: 1200px;
+  width: 100%;
+  min-height: 500px;
+}
+
+.cart-empty {
+  @include myFlex($jc: center);
+  background-color: white;
+  height: 561px;
+  border-radius: 5px;
+  gap: 30px;
+}
+
+.cart-full {
+  @include myFlex($ai: start);
+
+}
+</style>
