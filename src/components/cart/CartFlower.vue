77c7fb5b8d0f3088ffea76e00e66288c1daa3033
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-14 00:58:31 +0300

    Create CartFlower.vue

---
+++ src/components/cart/CartFlower.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="cart__flower flower">
+    <div class="flower__img-wrapper">
+      <img
+          class="flower__img"
+          :src="`src/assets/img/flowers/${flowerCart[0].img}`"
+          alt="flower">
+    </div>
+    <div class="flower__info">
+      <div class="flower__title-info">
+        <h3>{{ flowerCart[0].name }}</h3>
+        <button
+            class="flower__delete"
+            @click="deleteFlower()">
+          <img
+              src="src/assets/icons/delete.svg"
+              alt="delete">
+        </button>
+      </div>
+      <div class="flower__prices">
+        <p class="flower__unit-price">
+          unit price {{ flowerCart[0].price }}$
+        </p>
+        <div>
+          <CartFlowerCounter :flower-cart="props.flowerCart"/>
+          <p class="flower__total-uniq-price">
+            Total {{ flowerTotalPrice }}$
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CartFlowerCounter from "@/components/cart/CartFlowerCounter.vue";
+import {useCartStore} from "@/stores/cart";
+import {IFlower} from "@/interfaces/IFlower";
+import {computed} from "vue";
+
+const cartStore = useCartStore();
+
+const props = defineProps<{
+  flowerCart: [IFlower, number];
+}>();
+
+
+function deleteFlower() {
+  cartStore.removeFlower(props.flowerCart[0]);
+}
+
+const flowerTotalPrice = computed(() => {
+  return cartStore.getUniqFlowerTotalPrice(props.flowerCart[0]);
+});
+</script>
+
+<style scoped lang="scss">
+@import "@/mixins.scss";
+
+.cart__flower {
+  @include myFlex();
+  gap: 20px;
+
+  .flower__img-wrapper {
+    width: 200px;
+    height: 200px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    img {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .flower__info {
+    @include myFlex($fd: column, $ai: start);
+    max-width: 470px;
+    width: 100%;
+    max-height: 166px;
+    height: 100%;
+
+    .flower__title-info {
+      @include myFlex();
+      width: 100%;
+
+      h3 {
+        font-size: 28px;
+      }
+
+      .flower__delete {
+        background-color: white;
+        transform: scale(1.2, 1.2);
+        border: 0;
+        cursor: pointer;
+      }
+    }
+
+    .flower__prices {
+      @include myFlex($ai: start, $fd: column);
+      gap: 10px;
+      width: 100%;
+
+      .flower__unit-price {
+        font-size: 18px;
+        color: rgba(131, 131, 131, 1);
+      }
+
+      > div {
+        @include myFlex();
+        width: 100%;
+
+        .flower__total-uniq-price {
+          font-size: 20px;
+        }
+      }
+    }
+  }
+}
+</style>
