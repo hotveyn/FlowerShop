614f810d12511ae621546429c95df05014211220
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-13 20:57:16 +0300

    Update cart.ts

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -2,7 +2,7 @@ import {defineStore} from "pinia";
 import {IFlower} from "@/interfaces/IFlower";
 
 interface State {
-    flowersInCart: IFlower[];
+    flowersInCart: [IFlower, number][];
 }
 
 export const useCartStore = defineStore("cart", {
@@ -10,17 +10,38 @@ export const useCartStore = defineStore("cart", {
         flowersInCart: [],
     }),
     actions: {
+        addNewFlower(newFlower: IFlower): void {
+            const flowerInCart = this.flowersInCart.find((flower) =>
+                flower[0].id === newFlower.id,
+            );
+            if (flowerInCart) {
+                flowerInCart[1] += 1;
+            } else {
+                this.flowersInCart.push([newFlower, 1]);
+            }
+        },
+        removeFlower(flowerToRemove: IFlower): void {
+            this.flowersInCart.filter((flower) => {
+                return flower[0].id !== flowerToRemove.id;
+            });
+        },
+        changeFlowerCount(flowerToChange: IFlower, mod: 1 | -1): void {
+            const flowerInCart = this.flowersInCart.find((flower) =>
+                flower[0].id === flowerToChange.id,
+            );
+            if(flowerInCart){
+                flowerInCart[1] > 0 ? flowerInCart[1] += mod : this.removeFlower(flowerInCart[0]);
+            }
+        },
+
+    },
+    getters: {
+
+        getCartItems(): [IFlower, number][] {
+            return this.flowersInCart;
+        },
         getCartLength(): number {
             return this.flowersInCart.length;
-        },
-        addNewFlowerInCart(newFlower: IFlower): void {
-            this.flowersInCart.push(newFlower);
-        },
-        removeFlowerFromCart(flowerToRemove: IFlower): void {
-            //Fix: make more optimized
-            this.flowersInCart.filter((value) => {
-                return value !== flowerToRemove;
-            });
         },
     },
 });
