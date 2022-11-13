937253e284245abf7794472ad482249904fd7294
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-13 20:57:09 +0300

    Rename function and make it getter

--- src/stores/shop.ts
+++ src/stores/shop.ts
@@ -25,29 +25,33 @@ export const useShopStore = defineStore("shop", {
         },
         setReverse(newReverse: boolean): void {
             this.reverse = newReverse;
-        },
-        getFlowersBySortMod(sortMod: SortType): IFlower[] {
-            const flowerStore = useFlowersStore();
-
-            const tempFlowers: IFlower[] = [...flowerStore.flowers];
-
-            // Fix: Говнокод и наплевательское отношение к DRY, Сделать геттером
-            switch (sortMod) {
-                case "Popular":
-                    tempFlowers.sort((a, b) => (a.popular < b.popular) ? 1 : ((b.popular < a.popular) ? -1 : 0));
-                    break;
-                case "Name":
-                    tempFlowers.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
-                    break;
-                case "Rate":
-                    tempFlowers.sort((a, b) => (a.rate < b.rate) ? 1 : ((b.rate < a.rate) ? -1 : 0));
-                    break;
-                case "Price":
-                    tempFlowers.sort((a, b) => (a.price < b.price) ? 1 : ((b.price < a.price) ? -1 : 0));
-                    break;
-            }
-
-            return this.reverse ? tempFlowers.reverse() : tempFlowers;
-        },
+        }
     },
+    getters: {
+        getFlowersBySort: (state) =>{
+            return (sortMod: SortType) : IFlower[] =>{
+                const flowerStore = useFlowersStore();
+
+                const tempFlowers: IFlower[] = [...flowerStore.flowers];
+
+                // Fix: Говнокод и наплевательское отношение к DRY, Сделать геттером
+                switch (sortMod) {
+                    case "Popular":
+                        tempFlowers.sort((a, b) => (a.popular < b.popular) ? 1 : ((b.popular < a.popular) ? -1 : 0));
+                        break;
+                    case "Name":
+                        tempFlowers.sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0));
+                        break;
+                    case "Rate":
+                        tempFlowers.sort((a, b) => (a.rate < b.rate) ? 1 : ((b.rate < a.rate) ? -1 : 0));
+                        break;
+                    case "Price":
+                        tempFlowers.sort((a, b) => (a.price < b.price) ? 1 : ((b.price < a.price) ? -1 : 0));
+                        break;
+                }
+
+                return state.reverse ? tempFlowers.reverse() : tempFlowers;
+            };
+        }
+    }
 });
