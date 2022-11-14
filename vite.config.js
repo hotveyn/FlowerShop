2a8a4b0406e35183361d0e0000e8210a98b54d87
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-14 14:44:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 
 export default defineConfig({
-  resolve:{
-    alias:{
-      '@' : path.resolve(__dirname, './src')
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src")
+        },
     },
-  },
-  plugins: [vue()]
-})
+    plugins: [vue()]
+});
